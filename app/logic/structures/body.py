c147f52544e5127a9e2dd50c223f204cb754fd22
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2024-11-03 14:09:50 -0500

    fix: fix bad importation

--- app/logic/structures/body.py
+++ app/logic/structures/body.py
@@ -1,4 +1,5 @@
-from app.logic.vector import Vector3
+from app.logic.structures.vector import Vector3
+
 
 class Body:
     def __init__(
@@ -6,9 +7,14 @@ class Body:
         position: Vector3 = Vector3(),
         velocity: Vector3 = Vector3(),
         acceleration: Vector3 = Vector3(0, 0, 0),
-        mass: float = 3000000
+        mass: float = 3000000,
     ) -> None:
         self.position: Vector3 = position
-        self.velocity: Vector3= velocity
+        self.velocity: Vector3 = velocity
         self.acceleration: Vector3 = acceleration
         self.mass: float = mass
+
+    def adjust_position(self, height: float, width: float, depth: float) -> None:
+        self.position.x *= width
+        self.position.y *= height
+        self.position.z *= depth
