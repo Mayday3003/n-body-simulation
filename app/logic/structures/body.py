00a1b71dbb50dab352435dd60ae045204ed335bc
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2024-11-10 20:54:00 -0500

    fix: wrongs names in body class

--- app/logic/structures/body.py
+++ app/logic/structures/body.py
@@ -10,12 +10,12 @@ class Body:
         mass: float = 3000000,
     ) -> None:
         if position is None:
-            self.postiion = Vector2()
+            self.position = Vector2()
         else:
             self.position: Vector2 = position
 
         if velocity is None:
-            self.position = Vector2()
+            self.velocity = Vector2()
         else:
             self.velocity: Vector2 = velocity
         self.acceleration: Vector2 = acceleration
@@ -24,12 +24,3 @@ class Body:
     def adjust_position(self, height: float, width: float) -> None:
         self.position.x = ((self.position.x + 1) / 2) * width
         self.position.y = ((self.position.y + 1) / 2) * height
-
-if __name__ == '__main__':
-    bodies = [Body() for _ in range(3)]
-    other1 = Body()
-    print(other1.position)
-    other2 = Body()
-    print(other2.position)
-    for body in bodies:
-        print(body.position)
