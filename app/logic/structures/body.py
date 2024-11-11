afe7c4c05ea65a7d68c1ac14556dda909092f358
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2024-11-10 20:26:02 -0500

    fix: fixed init vectors in body

--- app/logic/structures/body.py
+++ app/logic/structures/body.py
@@ -4,16 +4,32 @@ from app.logic.structures.vector import Vector2
 class Body:
     def __init__(
         self,
-        position: Vector2 = Vector2(),
-        velocity: Vector2 = Vector2(),
+        position: Vector2 | None = None,
+        velocity: Vector2 | None = None,
         acceleration: Vector2 = Vector2(0, 0),
         mass: float = 3000000,
     ) -> None:
-        self.position: Vector2 = position
-        self.velocity: Vector2 = velocity
+        if position is None:
+            self.postiion = Vector2()
+        else:
+            self.position: Vector2 = position
+
+        if velocity is None:
+            self.position = Vector2()
+        else:
+            self.velocity: Vector2 = velocity
         self.acceleration: Vector2 = acceleration
         self.mass: float = mass
 
     def adjust_position(self, height: float, width: float) -> None:
         self.position.x = ((self.position.x + 1) / 2) * width
         self.position.y = ((self.position.y + 1) / 2) * height
+
+if __name__ == '__main__':
+    bodies = [Body() for _ in range(3)]
+    other1 = Body()
+    print(other1.position)
+    other2 = Body()
+    print(other2.position)
+    for body in bodies:
+        print(body.position)
