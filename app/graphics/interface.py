4e619734d7e486605a60f4d351cce569abbaec1f
Mayday <maydaycorrea@gmail.com>
2024-11-11 22:50:35 -0500

    feat: fix body color  and add cutoff for simulation

--- app/graphics/interface.py
+++ app/graphics/interface.py
@@ -1,13 +1,8 @@
-from time import sleep
+import time
 import pygame
 from app.logic.structures.vector import Vector2
 from app.logic.structures.body import Body
 from app.logic.simulation import Simulation
-from random import randint
-
-
-def random_color() -> tuple[int, int, int]:
-    return (randint(0, 255), randint(0, 255), randint(0, 255))
 
 
 class Interface:
@@ -18,17 +13,23 @@ class Interface:
         pygame.init()
         self.window = pygame.display.set_mode((width, height))
         self.simulation = Simulation(width=width, height=height)
-
+        
     def __draw_body(self, body: Body) -> None:
         pygame.draw.circle(
-            self.window, random_color(), (body.position.x, body.position.y), 5
+            self.window, body.color, (int(body.position.x), int(body.position.y)), 5
         )
 
     def init(self) -> None:
         running = True
+        start_time = time.time()  
 
         while running:
-            self.window.fill(Interface.BLACK)
+            time_elapsed = time.time() - start_time
+            if time_elapsed >= self.simulation.CUTTOF:
+                print("Simulation reached cutoff time.")
+                running = False
+                break
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
@@ -36,12 +37,12 @@ class Interface:
                     x, y = pygame.mouse.get_pos()
                     self.simulation.add_body(Body(position=Vector2(x, y)))
 
-
+            self.window.fill(Interface.BLACK)
             for body in self.simulation.bodies:
                 self.__draw_body(body)
 
             pygame.display.flip()
-
+            
             self.simulation.calculate_next_step()
 
     def finish(self):
--- app/logic/structures/body.py
+++ app/logic/structures/body.py
@@ -1,5 +1,5 @@
 from app.logic.structures.vector import Vector2
-
+import random
 
 class Body:
     def __init__(
@@ -8,6 +8,7 @@ class Body:
         velocity: Vector2 | None = None,
         acceleration: Vector2 = Vector2(0, 0),
         mass: float = 3000000,
+        color: tuple[int, int, int] = None,
     ) -> None:
         if position is None:
             self.position = Vector2()
@@ -20,6 +21,13 @@ class Body:
             self.velocity: Vector2 = velocity
         self.acceleration: Vector2 = acceleration
         self.mass: float = mass
+        self.color = color
+            
+        self.__post_init__()
+
+    def __post_init__(self):
+        if self.color is None:
+            self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
     def adjust_position(self, height: float, width: float) -> None:
         self.position.x = ((self.position.x + 1) / 2) * width
